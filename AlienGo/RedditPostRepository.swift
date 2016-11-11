cc7c77bc07f81ed3f2c9139bc03d3463b92bc1ef
Richmond Watkins <richmondwatkins@mac.com>
2016-11-10 18:47:00 -0600

    Move reddit repo to protocol

--- AlienGo/RedditPostRepository.swift
+++ AlienGo/RedditPostRepository.swift
@@ -10,7 +10,26 @@ import UIKit
 
 typealias RedditPostFetchCallback = (_ posts: [RedditPost]) -> Void
 
-class RedditPostRepository {
+protocol RedditPostRepository {
+    func getPostsFor(subreddit: Subreddit, callback: @escaping RedditPostFetchCallback)
+    func loadMore(postId: String, totalCount: Int, callback: @escaping RedditPostFetchCallback)
+    func deserializeRedditPostResponse(response: [[String: AnyObject]]) -> [RedditPost]
+}
+
+extension RedditPostRepository {
+    
+    func deserializeRedditPostResponse(response: [[String: AnyObject]]) -> [RedditPost] {
+        var posts: [RedditPost] = []
+        
+        posts.append(contentsOf: response.flatMap { (obj) in
+            return RedditPost(apiResponse: obj)
+        })
+        
+        return posts
+    }
+}
+
+class MainRedditRepository: RedditPostRepository {
 
     func getPostsFor(subreddit: Subreddit, callback: @escaping RedditPostFetchCallback) {
         NetworkManager.shared.getPostsForSubreddit(subreddit: subreddit) { (response, error) in
@@ -22,18 +41,7 @@ class RedditPostRepository {
             callback(self.deserializeRedditPostResponse(response: postResponse))
         }
     }
-    
-    func getFront(callback: @escaping RedditPostFetchCallback) {
-        NetworkManager.shared.getFrontPage { (response, error) in
-            guard let response = response, let postResponse = (response["data"] as? [String: AnyObject])?["children"] as? [[String: AnyObject]], error == nil else {
-                print("NONE AT FIRST PAGE")
-                return
-            }
-            
-            callback(self.deserializeRedditPostResponse(response: postResponse))
-        }
-    }
-    
+
     func loadMore(postId: String, totalCount: Int, callback: @escaping RedditPostFetchCallback) {
         NetworkManager.shared.getRedditPostsAtPage(lastPostId: postId, totalPostCount: totalCount) { (response, error) in
             print(response)
@@ -44,15 +52,5 @@ class RedditPostRepository {
             
             callback(self.deserializeRedditPostResponse(response: postResponse))
         }
-    }
-    
-    func deserializeRedditPostResponse(response: [[String: AnyObject]]) -> [RedditPost] {
-        var posts: [RedditPost] = []
-        
-        posts.append(contentsOf: response.flatMap { (obj) in
-            return RedditPost(apiResponse: obj)
-        })
-        
-        return posts
     }
 }
